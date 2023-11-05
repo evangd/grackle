5c67500148603265d8604ea49b6499a798964e64
Evan Dempsey <evangd@umich.edu>
2023-11-05 17:26:32 -0500

    Change PDO for live server

--- pdo.php
+++ pdo.php
@@ -1,5 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=grackle', 
-   'evan', 'bruh');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=evangdem_grackle', 
+    'evangdem_admin', 'Eightisgr8!');
+// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=grackle', 
+//    'evan', 'bruh');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
