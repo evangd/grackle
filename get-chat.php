754c20cae7fc027caf864a0dd11757543f275f97
Evan Dempsey <evangd@umich.edu>
2023-11-05 19:19:10 -0500

    Change sort order

--- get-chat.php
+++ get-chat.php
@@ -8,7 +8,7 @@ header('Content-Type: application/json; charset=utf-8');
 $stmt = $pdo->prepare('SELECT * FROM messages 
     INNER JOIN users ON messages.user_id = users.id
     WHERE messages.id >' . ' ' . strval($_SESSION['start']) . ' ' . 
-    'ORDER BY messages.id DESC');
+    'ORDER BY messages.id ASC');
 $stmt->execute();
 $return = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
