944f3b8ee857c2ff6e67062ef81c756180ca9a7d
Evan Dempsey <evangd@umich.edu>
2023-11-07 20:49:13 -0500

    Little touch-up

--- index.php
+++ index.php
@@ -107,12 +107,17 @@ if (!isset($_SESSION['id'])) {
                     // update user list
 
                     $('#users').empty();
-                    for (let i = 0; i < users.length; ++i) {
-                        let user = users[i];
 
-                        $('#users').append(`<li>${user['first_name']} ${user['last_name']}</li>`);
+                    if (users.length > 0 ) {
+                        for (let i = 0; i < users.length; ++i) {
+                            let user = users[i];
+
+                            $('#users').append(`<li>${user['first_name']} ${user['last_name']}</li>`);
+                        }
+                    } else {
+                        $('#users').append('<li>Just you!</li>');
                     }
-
+        
                     setTimeout(getNewChats(), 4000);
                 }
             });
