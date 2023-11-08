7040c1e75ad8a66e90d007f90304375ad7b89c42
Evan Dempsey <evangd@umich.edu>
2023-11-07 20:07:12 -0500

    Holy hell forgot PDO

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
 session_save_path('sessions');
 session_start();
 
+require_once 'pdo.php';
+
 $stmt = $pdo->prepare('UPDATE users SET online = 0 WHERE id = :uid');
 $stmt->execute(array(
     ':uid' => $_SESSION['id']
