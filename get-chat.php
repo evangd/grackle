8a916900a7b26cdc0dd024cdd855df0d2470894b
Evan Dempsey <evangd@umich.edu>
2023-11-09 20:34:37 -0500

    Fix get-chat query

--- get-chat.php
+++ get-chat.php
@@ -13,7 +13,7 @@ $stmt->execute();
 $msgs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $currUsers = $pdo->prepare('SELECT first_name, last_name 
-    FROM users WHERE online < :time AND id <> :uid');
+    FROM users WHERE online > :time AND id <> :uid');
 $currUsers->execute(array(
     ':time' => time() - 5
     ':uid' => $_SESSION['id']
