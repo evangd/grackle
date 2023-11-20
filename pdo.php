69ba9a12e0e814d264407542e713b20cb07ab196
Evan Dempsey <45055936+evangd@users.noreply.github.com>
2023-11-20 11:08:06 -0500

    Delete pdo.php
    Don't want people to see my database password

--- pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=evangdem_grackle', 
-    'evangdem_admin', 'Eightisgr8!');
-// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=grackle', 
-//    'evan', 'bruh');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
